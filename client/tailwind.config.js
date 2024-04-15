5e2e8b7c861f3c15b3da9e339b317c9c394be255
Shiraj Shrestha <shresthashiraj333@gmail.com>
2024-04-15 21:20:57 +0545

    Add Header Navbar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+// const flowbite = require("flowbite-react/tailwind");  this to code below
+import {
+  content as flowbiteContent,
+  plugin as flowbitePlugin,
+} from "flowbite-react/tailwind";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // flowbite.content()
+    flowbiteContent(),
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // flowbite.plugin()
+    flowbitePlugin(),
+  ],
 };
