f4e02cb9cef5bd3c32330dcf3f4f1bc885ea9862
Shiraj Shrestha <shresthashiraj333@gmail.com>
2024-04-16 23:44:27 +0545

    Connect MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(8000, () => {
+try {
+  mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+} catch (error) {
+  console.log(error);
+}
+
+app.listen(process.env.PORT, () => {
   console.log("Server started..");
 });
