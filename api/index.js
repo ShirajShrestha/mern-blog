001a9ab2f8ed12df7b42a5622e39beef8fc9291b
Shiraj Shrestha <shresthashiraj333@gmail.com>
2024-04-17 22:50:06 +0545

    Add signup api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoute.js";
+import authRoutes from "./routes/authRoute.js";
 
 const app = express();
 dotenv.config();
+
+app.use(express.json());
 
 // Database Connection
 try {
@@ -11,6 +15,10 @@ try {
 } catch (error) {
   console.log(error);
 }
+
+// Routes
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server started..");
